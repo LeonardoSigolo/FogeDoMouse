8d5598d7bcc8aaddb364ab928f4913a47565c1d4
Leonardo <leonardo.sigolo1037@gmail.com>
2026-09-10 13:47:58 -0300

    1.0 do joguinho

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/** @type {HTMLElement} */
+const isca = document.querySelector(".isca");
+
+const random = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+let tentativas = 0;
+
+const contador = () => {
+  tentativas++;
+  localStorage.setItem("tentivas", tentativas);
+};
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const rect = isca.getBoundingClientRect();
+
+  const xTelaMax = window.innerWidth - rect.width;
+  const yTelaMax = window.innerHeight - rect.height;
+
+  const xIscaMin = rect.left;
+  const xIscaMax = rect.right;
+
+  const yIscaMin = rect.top;
+  const yIscaMax = rect.bottom;
+
+  if (x > xIscaMin && x < xIscaMax && y > yIscaMin && y < yIscaMax) {
+    const xRand = random(xTelaMax, 0);
+    const yRand = random(yTelaMax, 0);
+    isca.style.left = `${xRand}px`;
+    isca.style.top = `${yRand}px`;
+    contador();
+  }
+
+  console.log(`x:${x} y:${y}`);
+  console.log(xIscaMin, xIscaMax, yIscaMin, yIscaMax);
+  console.log(xTelaMax, yTelaMax);
+});
