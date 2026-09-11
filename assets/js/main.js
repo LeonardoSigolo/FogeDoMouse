329e6935f21a4cf76e0454a7b95771c77da14e92
Leonardo <leonardo.sigolo1037@gmail.com>
2026-09-11 02:34:00 -0300

    1.5 add: ultima posição e emojis salvos

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,25 +45,43 @@ const salvaTentativas = () => {
   localStorage.setItem("tentativas", tentativas);
 };
 
+const salvaPosicaoEmoji = (xRand, yRand) => {
+  const emojiAtual = isca.innerText;
+
+  localStorage.setItem("xRand", xRand);
+  localStorage.setItem("yRand", yRand);
+  localStorage.setItem("emoji", emojiAtual);
+};
+
+const getPosicaoEmoji = () => {
+  const xRand = localStorage.getItem("xRand");
+  const yRand = localStorage.getItem("yRand");
+  const emoji = localStorage.getItem("emoji");
+
+  isca.style.left = `${xRand}px`;
+  isca.style.top = `${yRand}px`;
+  isca.innerText = emoji;
+};
+
+getPosicaoEmoji();
+
 let mouseEmCima = false;
 
-
 document.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    
-    const rect = isca.getBoundingClientRect();
-    
-    const xTelaMax = window.innerWidth - rect.width;
-    const yTelaMax = window.innerHeight - rect.height;
-    
-    const xIscaMin = rect.left;
-    const xIscaMax = rect.right;
-    
-    const yIscaMin = rect.top;
-    const yIscaMax = rect.bottom;
-    
-    const emCima = x > xIscaMin && x < xIscaMax && y > yIscaMin && y < yIscaMax;
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const rect = isca.getBoundingClientRect();
+  const xTelaMax = window.innerWidth - rect.width;
+  const yTelaMax = window.innerHeight - rect.height;
+
+  const xIscaMin = rect.left;
+  const xIscaMax = rect.right;
+
+  const yIscaMin = rect.top;
+  const yIscaMax = rect.bottom;
+
+  const emCima = x > xIscaMin && x < xIscaMax && y > yIscaMin && y < yIscaMax;
 
   if (emCima && !mouseEmCima) {
     const xRand = random(xTelaMax, 0);
@@ -71,15 +89,14 @@ document.addEventListener("mousemove", (e) => {
     isca.style.left = `${xRand}px`;
     isca.style.top = `${yRand}px`;
     const randEmoji = emojis[random(emojis.length - 1, 0)];
-    mouseEmCima = true
     trocaEmoji(randEmoji);
+    mouseEmCima = true;
     salvaTentativas();
-    console.log(mouseEmCima)
+    salvaPosicaoEmoji(xRand, yRand);
+    console.log(mouseEmCima);
   }
 
-  if(!emCima){
-    mouseEmCima = false;
-  }
+  if (!emCima) mouseEmCima = false;
   //   console.log(`x:${x} y:${y}`);
   //   console.log(xIscaMin, xIscaMax, yIscaMin, yIscaMax);
   //   console.log(xTelaMax, yTelaMax);
