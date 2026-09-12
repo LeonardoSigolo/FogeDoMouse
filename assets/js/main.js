568253c95eeeff367ed9b7f8b06818ee1798e65d
Leonardo <leonardo.sigolo1037@gmail.com>
2026-09-12 01:31:34 -0300

    Alteração dos emojis e cor de fundo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ const random = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
 const emojis = [
   "💖",
   "😍",
-  "🤡",
   "🤢",
   "😡",
   "🥵",
@@ -24,13 +23,25 @@ const emojis = [
   "🐵",
   "🫦",
   "👀",
-  "👨‍🦽‍➡️",
   "🏃‍♂️‍➡️",
   "💃",
   "🖖",
   "🤏",
   "🖕",
-  "👉👌",
+  "😎",
+  "😤",
+  "😭",
+  "🤪",
+  "👻",
+  "😽",
+  "🐸",
+  "🦝",
+  "🐽",
+  "🐎",
+  "🦕",
+  "🦖",
+  "🦋",
+  "🐓",
 ];
 
 // const trocaEmoji = () => {
@@ -69,35 +80,35 @@ const emojis = [
 // let mouseEmCima = false;
 
 // document.addEventListener("mousemove", (e) => {
-  // const x = e.clientX;
-  // const y = e.clientY;
+// const x = e.clientX;
+// const y = e.clientY;
 
-  // const rect = isca.getBoundingClientRect();
-  // const xTelaMax = window.innerWidth - rect.width;
-  // const yTelaMax = window.innerHeight - rect.height;
+// const rect = isca.getBoundingClientRect();
+// const xTelaMax = window.innerWidth - rect.width;
+// const yTelaMax = window.innerHeight - rect.height;
 
-  // const xIscaMin = rect.left;
-  // const xIscaMax = rect.right;
+// const xIscaMin = rect.left;
+// const xIscaMax = rect.right;
 
-  // const yIscaMin = rect.top;
-  // const yIscaMax = rect.bottom;
+// const yIscaMin = rect.top;
+// const yIscaMax = rect.bottom;
 
-  // const emCima = x > xIscaMin && x < xIscaMax && y > yIscaMin && y < yIscaMax;
+// const emCima = x > xIscaMin && x < xIscaMax && y > yIscaMin && y < yIscaMax;
 
-  // if (emCima && !mouseEmCima) {
-  //   const xRand = random(xTelaMax, 0);
-  //   const yRand = random(yTelaMax, 0);
-  //   isca.style.left = `${xRand}px`;
-  //   isca.style.top = `${yRand}px`;
-    // trocaEmoji();
-    // mouseEmCima = true;
-    // salvaTentativas();
-    // salvaPosicaoEmoji(xRand, yRand);
-    // console.log(mouseEmCima);
-  // }
+// if (emCima && !mouseEmCima) {
+//   const xRand = random(xTelaMax, 0);
+//   const yRand = random(yTelaMax, 0);
+//   isca.style.left = `${xRand}px`;
+//   isca.style.top = `${yRand}px`;
+// trocaEmoji();
+// mouseEmCima = true;
+// salvaTentativas();
+// salvaPosicaoEmoji(xRand, yRand);
+// console.log(mouseEmCima);
+// }
 
-  // if (!emCima) mouseEmCima = false;
-  //   console.log(`x:${x} y:${y}`);
-  //   console.log(xIscaMin, xIscaMax, yIscaMin, yIscaMax);
-  //   console.log(xTelaMax, yTelaMax);
+// if (!emCima) mouseEmCima = false;
+//   console.log(`x:${x} y:${y}`);
+//   console.log(xIscaMin, xIscaMax, yIscaMin, yIscaMax);
+//   console.log(xTelaMax, yTelaMax);
 // });
